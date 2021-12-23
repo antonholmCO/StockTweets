9f86fdd3584e4fd469bf7fc40b8f9bd44d7e0189
Anton <anton.holm.y@gmail.com>
2021-12-23 12:41:29 +0100

    Changed project structure so spring boot could be built via maven

--- src/main/java/online/stocktweets/StockTweets/controller/APIController.java
+++ src/main/java/online/stocktweets/StockTweets/controller/APIController.java
@@ -13,13 +13,13 @@ public class APIController {
 
 
 
-    @GetMapping("/tweets")
-    public List getTweets() {
+    @GetMapping("/tweets/{stockSymbol}")
+    public List getTweets(@PathVariable String stockSymbol) {
         TweetService ts = new TweetService();
 //        ArrayList<Tweet> tweets = ts.getTweetsFromTwitter("1228393702244134912,1227640996038684673,1199786642791452673");
         Tweet t2 = null;
         try {
-            t2 = ts.getTweets("");
+            t2 = ts.getTweets(stockSymbol);
         } catch (Exception e) {
         }
 
